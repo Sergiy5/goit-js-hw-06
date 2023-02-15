95de2de4be99d6f961144990e82928ce6dcbe78b
Sergiy Kushnir <s1983kushnir@gmail.com>
2023-02-15 11:07:19 +0200

    Fix first hw

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,8 @@
 const categories = document.querySelectorAll(".item");
-const namesCategories = document.querySelectorAll('h2');
+// const namesCategories = document.querySelectorAll('h2');
 
 console.log(`Number of categories: ${categories.length}`);
-console.log(numberOfCategories);
+// console.log(numberOfCategories);
 // Items
 
 const firstItem = document.querySelector(".item");
@@ -17,32 +17,22 @@ const lastItem = secondItem.nextElementSibling;
 // console.log(lastItem);
 // categories[2];
 
-// function nameAndNumberOfCategory(nameOfCategory) {
-for (let i = 0; i < namesCategories.length; i += 1) {
-        
-    console.log((`Category: ${namesCategories[i].textContent}`));
-    
-        console.log(
-          `Elements: ${namesCategories[i].querySelector("li").length}`
-        );
-    }
-// }
-// nameAndNumberOfCategory(namesCategories);
 
-// const firstTitle = firstItem.querySelector("h2");
-// console.log(`Category: ${firstTitle.textContent}`);
 
-// const countOfFirstEl = firstItem.querySelectorAll("li");
-// console.log(`Elements: ${countOfFirstEl.length}`);
+const firstTitle = firstItem.querySelector("h2");
+console.log(`Category: ${firstTitle.textContent}`);
 
-// const secondTitle = secondItem.querySelector("h2");
-// console.log(`Category: ${firstTitle.textContent}`);
+const countOfFirstEl = firstItem.querySelectorAll("li");
+console.log(`Elements: ${countOfFirstEl.length}`);
 
-// const countOfSecondEl = secondItem.querySelectorAll("li");
-// console.log(`Elements: ${countOfSecondEl.length}`);
+const secondTitle = secondItem.querySelector("h2");
+console.log(`Category: ${firstTitle.textContent}`);
 
-// const lastTitle = lastItem.querySelector("h2");
-// console.log(`Category: ${firstTitle.textContent}`);
+const countOfSecondEl = secondItem.querySelectorAll("li");
+console.log(`Elements: ${countOfSecondEl.length}`);
 
-// const countOfLastEl = lastItem.querySelectorAll("li");
-// console.log(`Elements: ${countOfLastEl.length}`);
+const lastTitle = lastItem.querySelector("h2");
+console.log(`Category: ${firstTitle.textContent}`);
+
+const countOfLastEl = lastItem.querySelectorAll("li");
+console.log(`Elements: ${countOfLastEl.length}`);
