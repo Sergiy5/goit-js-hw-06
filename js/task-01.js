e31399f9433a4f5ab31d85b80ba6480d3675ba82
Sergiy Kushnir <s1983kushnir@gmail.com>
2023-02-17 14:05:55 +0200

    Dun all

--- js/task-01.js
+++ js/task-01.js
@@ -2,11 +2,21 @@ const categories = document.querySelectorAll(".item");
 
 console.log(`Number of categories: ${categories.length}`);
 
-for (let i = 0; i < categories.length; i += 1) {
-
-    console.log(`Category: ${categories[i].firstElementChild.textContent}`);
-
-    const arrLength = categories[i].lastElementChild;
+categories.forEach((categorie) => {
+    console.log('')
+    console.log(`Category:`, categorie.firstElementChild.textContent);
+    console.log(`Elements:`, categorie.lastElementChild.children.length);
     
-    console.log(`Elements: ${arrLength.querySelectorAll('li').length}`);
-};
+});
+
+
+
+
+// for (let i = 0; i < categories.length; i += 1) {
+
+//     console.log(`Category: ${categories[i].firstElementChild.textContent}`);
+
+//     const arrLength = categories[i].lastElementChild;
+    
+//     console.log(`Elements: ${arrLength.querySelectorAll('li').length}`);
+// };
